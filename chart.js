54a40a210c3710becbbd503e2f128ae53756a01b
epicommu <160574448+epicommu@users.noreply.github.com>
2024-03-29 16:54:24 +0900

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Investment Growth Visualization</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+    <div>
+        <canvas id="investmentChart"></canvas>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
